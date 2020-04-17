52aac6fe2f0c8e7162a4570c81d6b6048a6c54f5
Skytells, Inc <support@skytells.net>
2020-04-17 23:43:47 +0200

    Initial

---
+++ Covid19 Detection/Covid19 Detection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19 Detection
+//
+//  Created by Hazem Ali on 4/17/20.
+//  Copyright © 2020 Skytells, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
